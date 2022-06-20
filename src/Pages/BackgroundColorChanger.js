fb0bfb06efc9e9f62c3af34cea5040d165140971
Ayush Saraswat <saraswatayush25@gmail.com>
2022-06-20 20:45:54 +0530

    message

--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-
-export const BackgroundColorChanger = () =>{
-    return(
-       
-    )
-}
+export const BackgroundColorChanger = () => {
+  const [bg, setBg] = useState("");
+  const params = useParams();
+  useEffect(() => {
+    setBg(params.colorname);
+    console.log(bg);
+  }, [bg]);
+  return <main style={{ height: "100vh", backgroundColor: `${bg}` }}></main>;
+};
--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
+function operatorResult(a, b, op) {
+  switch (op) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+    case "%":
+      return a % b;
 
-
-export const Calculator = () =>{
-
-    return(
-        
-    )
+    default:
+      return;
+  }
 }
+export const Calculator = () => {
+  const [search, setSearch] = useSearchParams();
+  const num1 = search.get("num1");
+  const operator = search.get("op");
+  const num2 = search.get("num2");
+
+  const result = operatorResult(Number(num1), Number(num2), operator);
+  console.log(num1);
+  return (
+    <div>
+      Your calculation Result is <span id="calc-result">{result}</span>
+    </div>
+  );
+};
